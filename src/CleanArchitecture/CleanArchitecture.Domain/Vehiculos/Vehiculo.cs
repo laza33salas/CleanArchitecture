acddef95ca93c9938fbd538539721e1cd9ccd49c
Lazaro Salas <lazarocode@gmail.com>
2025-12-17 17:28:36 -0300

    Remplazo valores primitivos por Object Values

--- src/CleanArchitecture/CleanArchitecture.Domain/Vehiculos/Vehiculo.cs
+++ src/CleanArchitecture/CleanArchitecture.Domain/Vehiculos/Vehiculo.cs
@@ -1,18 +1,19 @@
+using CleanArchitecture.Domain.Abstractions;
+
 namespace CleanArchitecture.Domain.Vehiculos;
 
-public sealed class Vehiculos
+public sealed class Vehiculo : Entity
 {
-   public Guid Id { get; private set; }
-   public string? Modelo { get; private set; }
-   public string? Vin { get; private set; } //Número de serie
-   public string? Calle { get; private set; }
-   public string? Departamento { get; private set; }
-   public string? Ciudad { get; private set; }
-   public string? Pais { get; private set; }
+   public Vehiculo(Guid id) : base(id)
+   { }
+   public Direccion? Direccion { get; private set; }
+   public Modelo? Modelo { get; private set; }
+   public Vin? Vin { get; private set; } //Número de serie
    public decimal Precio { get; private set; }
    public string? TipoMoneda { get; private set; }
    public decimal Mantenimiento { get; private set; }
    public string? MantenimientoTipoMoneda { get; private set; }
    public DateTime? FechaUltimoAlquiler { get; private set; }
+   public List<Accesorio> Accesorios { get; private set; } = new();
 
 }

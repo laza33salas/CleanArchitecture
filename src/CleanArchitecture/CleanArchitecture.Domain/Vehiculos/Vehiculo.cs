c62319f4006c13707e474b997e41758d3ad0b2de
Lazaro Salas <lazarocode@gmail.com>
2025-12-18 18:29:38 -0300

    factory method Users

--- src/CleanArchitecture/CleanArchitecture.Domain/Vehiculos/Vehiculo.cs
+++ src/CleanArchitecture/CleanArchitecture.Domain/Vehiculos/Vehiculo.cs
@@ -1,18 +1,41 @@
+using System.Runtime.CompilerServices;
 using CleanArchitecture.Domain.Abstractions;
 
 namespace CleanArchitecture.Domain.Vehiculos;
 
 public sealed class Vehiculo : Entity
 {
-   public Vehiculo(Guid id) : base(id)
-   { }
+   public Vehiculo(
+      Guid id,
+      Modelo modelo,
+      Vin vin,
+      Moneda precio,
+      Moneda mantenimiento,
+      DateTime? fechaUltimoAlquiler,
+      List<Accesorio> accesorios,
+      Direccion direccion
+
+
+      ) : base(id)
+   {
+      Modelo = modelo;
+      Vin = vin;
+      Precio = precio;
+      Mantenimiento = mantenimiento;
+      FechaUltimoAlquiler = fechaUltimoAlquiler;
+      Accesorios = accesorios;
+      Direccion = direccion;
+
+
+   }
+
+
+
    public Direccion? Direccion { get; private set; }
    public Modelo? Modelo { get; private set; }
    public Vin? Vin { get; private set; } //Número de serie
-   public decimal Precio { get; private set; }
-   public string? TipoMoneda { get; private set; }
-   public decimal Mantenimiento { get; private set; }
-   public string? MantenimientoTipoMoneda { get; private set; }
+   public Moneda? Precio { get; private set; }
+   public Moneda? Mantenimiento { get; private set; }
    public DateTime? FechaUltimoAlquiler { get; private set; }
    public List<Accesorio> Accesorios { get; private set; } = new();
 

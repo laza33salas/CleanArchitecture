aa0994691fc23f987aa290ef3fd27545d88de872
Lazaro Salas <lazarocode@gmail.com>
2026-01-21 17:18:42 -0300

    Remplazo los valores primitivos en las propiedades por los records Review y Rating en la Entidad Review

--- src/CleanArchitecture/CleanArchitecture.Domain/Reviews/Review.cs
+++ src/CleanArchitecture/CleanArchitecture.Domain/Reviews/Review.cs
@@ -1,1 +1,65 @@
+using System.Reflection.Metadata.Ecma335;
+using CleanArchitecture.Domain.Abstractions;
+using CleanArchitecture.Domain.Alquileres;
+using CleanArchitecture.Domain.Reviews.Events;
+using CleanArchitecture.Domain.Vehiculos;
+
 namespace CleanArchitecture.Domain.Reviews;
+
+public sealed class Review : Entity
+{
+
+    private Review(
+        Guid id,
+        Guid vehiculoId,
+        Guid alquilerId,
+        Guid userId,
+        Rating rating,
+        Review comentario,
+        DateTime fechaCreacion
+        ) : base(id)
+    {
+        VehiculoId = vehiculoId;
+        AlquilerId = alquilerId;
+        UserId = userId;
+        Rating = rating;
+        Comentario = comentario;
+        FechaCreacion = fechaCreacion;
+    }
+
+    public Guid VehiculoId { get; private set; }
+    public Guid UserId { get; private set; }
+    public Guid AlquilerId { get; private set; }
+    public Rating Rating { get; private set; }
+    public Review Comentario { get; private set; }
+    public DateTime FechaCreacion { get; private set; }
+
+
+    public static Result<Review> Create(
+       Alquiler alquiler,
+       Rating rating,
+       Review comentario,
+       DateTime fechaCreacion
+        )
+    {
+        if (alquiler.Status != AlquilerStatus.Completado)
+        {
+            return Result.Failure<Review>(ReviewErrors.NotEligible);
+        }
+
+        var review = new Review(
+            Guid.NewGuid(),
+            alquiler.VehiculoId,
+            alquiler.Id,
+            alquiler.UserId,
+            rating,
+            comentario,
+            fechaCreacion
+            );
+
+
+        review.RaiseDomainEvent(new ReviewCreatedDomainEvent(review.Id));
+        return review;
+    }
+
+}

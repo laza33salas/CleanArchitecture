18e70a0b66594144325512fa2cda1db07720ac37
Lazaro Salas <lazarocode@gmail.com>
2026-01-23 17:06:24 -0300

    ICommandHanlder

---
+++ src/CleanArchitecture/CleanArchitecture.Application/Abstractions/Messaging/ICommandHandler.cs
@@ -0,0 +1,16 @@
+using CleanArchitecture.Domain.Abstractions;
+using MediatR;
+
+namespace CleanArchitecture.Application.Abstractions.Messaging;
+
+public interface IQueryHandler<TCommand> : IRequestHandler<TCommand, Result>
+where TCommand : ICommand
+{
+
+}
+
+public interface ICommandHandler<TCommand, TResponse>
+: IRequestHandler<TCommand, Result<TResponse>>
+where TCommand : ICommand<TResponse>
+{
+}
